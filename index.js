12ef0c49d0d578bdc76f12aab95a90197778fe5f
FloraMMoon <36046672+FloraMMoon@users.noreply.github.com>
2019-03-10 15:40:23 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var List = require("collections/list");
 
-var totalUsers = 0;
-var userList = [];
-var colourDict = {};
-var mssgCount = 0;
+var totalConnections = 0; // number of connections since server started
+var userList = [];  // list of users currently in chat
+var colourDict = {};    //users who required a colored nickname, and the color itself
+var messages = [];
 
 app.get('/', function(req,res) {
-   res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 // Handle a connection
 io.on('connection', function(socket) {
-    // If client is first user, check length of storage of old messages
-    if (totalUsers === 0) {
-        console.log("1st user !!!!");
-        io.to(`${socket.id}`).emit('check storage');
-    }
-
-    // Handle this new client connection
     console.log('A user connected');
     console.log(socket.id);
-    // increment total number of users for username creation
-    totalUsers += 1;
-    let userId = "User"+totalUsers;
-    // update list of current users
-    userList.add(userId);
-    // send new client their username
-    io.to(`${socket.id}`).emit('new connection', userId);
-    // send updated list to all clients
-    io.emit('changed user list', userList);
+    // Increment total number of connections
+    totalConnections += 1;
+    // Create a userId for this client
+    let userId = "";
+
+    // Client is a returning user
+    socket.on('returning user', function(nickname) {
+        userId = nickname;
+        userJoin (socket, userId)
+    });
+
+    // Client is a new user
+    socket.on('new user', function() {
+        userId = "User"+totalConnections;
+        userJoin (socket, userId)
+    });
 
     // Handle this client disconnection
     socket.on('disconnect', function(){
@@ -41,12 +41,6 @@ io.on('connection', function(socket) {
         userList.delete(userId);
         // send updated list to all clients
         io.emit('changed user list', userList);
-    });
-
-    // Very first client sent size of local storage
-    socket.on('storage length', function(count){
-        mssgCount = count;
-        console.log("There were already " + mssgCount + " messages");
     });
 
     // Client sent a message
@@ -67,80 +61,87 @@ io.on('connection', function(socket) {
         else {
             // Check if user has a nickname colour attributed
             if (socket.id in colourDict) {
-                io.emit('chat message', msg, time, userId, colourDict[socket.id], socket.id, mssgCount);
+                io.emit('chat message', msg, time, userId, colourDict[socket.id], socket.id);
             }
-            // Else send message in black
+            // Else send black
             else {
-                io.emit('chat message', msg, time, userId, '000000', socket.id, mssgCount);
+                io.emit('chat message', msg, time, userId, '000000', socket.id);
             }
-            // Increment message count
-            mssgCount += 1;
-            // If localStorage  of messages has become too big, have this client fix it
-            /*if (mssgCount > 10) {
-                console.log(mssgCount + " messages");
-                io.to(`${socket.id}`).emit('renew localStorage');
-            }*/
+        }
+    });
+
+    // Client formed a message
+    socket.on('store message', function(msgString) {
+        messages.push(msgString);
+        if (messages.length >= 400) {
+            messages = messages.slice (199);
         }
     });
 });
 
-    http.listen(3000, function() {
+
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
-    // Function handling client command to change their username colour
-    function colourChange (socket, colour) {
-        // Check if valid rrggbb colour
-        // is it 6 digits long?
-        if (colour.length === 6) {
-            // is it a valid hex number?
-            // parse in hex
-            let translatedcolour = parseInt(colour, 16);
-            // back to decimal string
-            translatedcolour = translatedcolour.toString(16);
-            // pad with leading zeros if necessary
-            while (translatedcolour.length < 6) {
-                translatedcolour = "0" + translatedcolour;
-            }
-            // compare with original string
-            if (translatedcolour === colour){
-                // Add valid colour to dictionary
-                colourDict[socket.id] = colour;
-                // Erase any message in com area
-                io.to(`${socket.id}`).emit('valid colour');
-            }
-            else {
-                // Tell user that colour is invalid
-                io.to(`${socket.id}`).emit('colour not valid', colour);
-            }
+//Function handling recent connection
+function userJoin (socket, userId) {
+    // update list of current users
+    userList.add(userId);
+    // send new client their username and messages history
+    io.to(`${socket.id}`).emit('new connection', userId, messages);
+    // send updated list to all clients
+    io.emit('changed user list', userList);
+}
+
+// Function handling client command to change their username colour
+function colourChange (socket, colour) {
+    // Check if valid rrggbb colour
+    // is it 6 digits long?
+    if (colour.length === 6) {
+        // is it a valid hex number?
+        // parse in hex
+        let translatedcolour = parseInt(colour, 16);
+        // back to decimal string
+        translatedcolour = translatedcolour.toString(16);
+        // pad with leading zeros if necessary
+        while (translatedcolour.length < 6) {
+            translatedcolour = "0" + translatedcolour;
+        }
+        // compare with original string
+        if (translatedcolour === colour.toLowerCase()){
+            // Add valid colour to dictionary
+            colourDict[socket.id] = colour;
+            // Erase any message in com area
+            io.to(`${socket.id}`).emit('valid colour');
         }
         else {
             // Tell user that colour is invalid
             io.to(`${socket.id}`).emit('colour not valid', colour);
         }
     }
-
-    // Function handling client command to change their username
-    function usernameChange(socket, username, userId){
-        console.log("desired: " + username);
-        var lowerCaseList = [];
-        for (i=0; i<userList.length; i++) {
-            lowerCaseList.add(userList[i].toLowerCase());
-        }
-        console.log(lowerCaseList);
-        if (lowerCaseList.indexOf(username.toLowerCase()) >= 0) {
-            io.to(`${socket.id}`).emit('nickname not unique', username);
-            return userId;
-        }
-        else {
-            console.log("unique");
-            userList.delete(userId);
-            console.log(userList);
-            userId = username;
-            userList.add(userId);
-            console.log(userList);
-            io.to(`${socket.id}`).emit('nickname unique', username);
-            io.emit('changed user list', userList);
-            return userId;
-        }
+    else {
+        // Tell user that colour is invalid
+        io.to(`${socket.id}`).emit('colour not valid', colour);
     }
+}
+
+// Function handling client command to change their username
+function usernameChange(socket, username, userId){
+    var lowerCaseList = [];
+    for (i=0; i<userList.length; i++) {
+        lowerCaseList.add(userList[i].toLowerCase());
+    }
+    if (lowerCaseList.indexOf(username.toLowerCase()) >= 0) {
+        io.to(`${socket.id}`).emit('nickname not unique', username);
+        return userId;
+    }
+    else {
+        userList.delete(userId);
+        userId = username;
+        userList.add(userId);
+        io.to(`${socket.id}`).emit('nickname unique', username);
+        io.emit('changed user list', userList);
+        return userId;
+    }
+}
